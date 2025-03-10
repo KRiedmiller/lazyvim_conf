189907fbb36241ee71a1d8f8c729f82699347a39
Kai Riedmiller <kai.riedmiller@web.de>
2025-03-10 14:42:25 +0100

    keymap updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,7 @@
 -- Add any additional keymaps here
 -- Terminal Mappings
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Hide Terminal" })
+vim.keymap.set("n", "oo", "o<Esc>k", { desc = "add newline below" })
+vim.keymap.set("n", "OO", "O<Esc>j", { desc = "add newline above" })
+vim.keymap.set("i", "<C-j>", "<Esc><C-j>", { remap = true, desc = "switch windows in insert mode" })
+vim.keymap.set("i", "<C-k>", "<Esc><C-k>", { remap = true, desc = "switch windows in insert mode" })
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -24,6 +24,8 @@ return {
         ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
         ["<C-y>"] = { "select_and_accept" },
         ["<C-e>"] = { "hide", "fallback" },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
       },
       completion = {
         list = {
