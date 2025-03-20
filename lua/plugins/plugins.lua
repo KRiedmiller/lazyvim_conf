db528a674f1e64fbaf140149736f36aec7720978
Kai Riedmiller <kai.riedmiller@web.de>
2025-03-20 10:28:25 +0100

    update

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,5 +1,24 @@
 return {
   {
+    "folke/noice.nvim",
+    keys = {
+      -- change hover scroll
+      { "<c-b>", false, mode = { "i", "n", "s" } },
+      {
+        "<c-g>",
+        function()
+          if not require("noice.lsp").scroll(-4) then
+            return "<c-g>"
+          end
+        end,
+        silent = true,
+        expr = true,
+        desc = "Scroll Backward",
+        mode = { "i", "n", "s" },
+      },
+    },
+  },
+  {
     "echasnovski/mini.surround",
     opts = {
       mappings = {
@@ -24,7 +43,7 @@ return {
         ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
         ["<C-y>"] = { "select_and_accept" },
         ["<C-e>"] = { "hide", "fallback" },
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-g>"] = { "scroll_documentation_up", "fallback" },
         ["<C-f>"] = { "scroll_documentation_down", "fallback" },
       },
       completion = {
